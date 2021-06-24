7214dba0a910df44da9e65dcc155fafcda8aec24
zhaox1n <zhao.x1n@foxmail.com>
2021-06-25 01:56:21 +0800

    init commit

--- common/datablocks/src/kernels/data_block_group_by.rs
+++ common/datablocks/src/kernels/data_block_group_by.rs
@@ -16,7 +16,7 @@ pub type GroupIndicesTable = HashMap<Vec<u8>, (Vec<u32>, Vec<DataValue>), ahash:
 type GroupBlocksTable = Vec<(Vec<u8>, Vec<DataValue>, DataBlock)>;
 
 pub type VecGroupTable = HashMap<u64, Vec<u32>, IdHashBuilder>;
-type VecGroupBlockTable = Vec<(u64, Vec<u32>, DataBlock)>;
+type VecGroupBlockTable = Vec<(u64, Vec<DataValue>, DataBlock)>;
 
 impl DataBlock {
     /// Hash group based on row index then return indices and keys.
@@ -129,16 +129,39 @@ impl DataBlock {
         // 2. Make group with indices.
         {
             for row in 0..block.num_rows() {
-                let group_key = hashes.get(row).unwrap();
-
                 match group_indices.get_mut(&group_key) {
                     None => {
-                        group_indices.insert(group_key.clone(), vec![row as u32]);
+                        group_indices.insert(group_key.clone(), (vec![row as u32]);
                     }
                     Some(v) => {
                         v.push(row as u32);
                     }
                 }
+            }
+        }
+
+        fn get_group_key(row: usize) -> Vec<DataValue> {
+            let mut group_keys = Vec::with_capacity(group_key.len());
+            for col in &group_columns {
+                group_keys.push(DataValue::try_from_column(col, row)?);
+            }
+            group_keys
+        }
+
+        // 3. Make Group block
+        let mut group_blocks = VecGroupBlockTable::default();
+        {
+            for (group_key, group_indices, group_keys) in group_indices {
+                let mut keyFlag: usize = 0;
+                let mut meetDifferent: bool = false;
+                let group_keys = get_group_key(keyFlag);
+                while keyFlag <= group_indices.len() {
+                    
+                }
+                let mut group_item = Vec::default();
+
+                let take_block = DataBlock::block_take_by_indices(&block, &group_indices)?;
+                group_blocks.push((group_key, group_keys, take_block));
             }
         }
 
